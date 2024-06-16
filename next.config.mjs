f4218782184271db9b7490076046858381e69ae9
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2024-06-16 01:16:21 -0500

    v0.0.16
    Testing PWA support

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+import withPWA from 'next-pwa';
+import runtimeCaching from 'next-pwa/cache.js';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const nextConfig = withPWA({
+  dest: 'public',
+  disable: isDev,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  register: true,
+  skipWaiting: true,
+  sw: 'sw.js',
+  clientsClaim: true,
+  skipWaiting: true,
+});
 
 export default nextConfig;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="manifest" href="manifest.json" />
+      <link rel="manifest" href="../../manifest.json" />
       </Head>
       <body>
         <Main />
