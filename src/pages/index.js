142e925c89bf3950131e91a7bad8537267c18867
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2024-06-15 23:27:01 -0500

    v0.0.5
    Set hardcoded with for invoice textarea

--- src/pages/index.js
+++ src/pages/index.js
@@ -173,7 +173,7 @@ const Wallet = () => {
 
       <div className="cashu-operations-container">
 
-      <h1>BullishNuts</h1><p>v0.0.4</p>
+      <h1>BullishNuts</h1><p>v0.0.5</p>
 
         <div className="section">
           <h2>Balance</h2>
