6aa7fa40e0ca0431821c2da55de262597cb1051b
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2024-06-16 21:36:51 -0500

    v0.0.19
    Moved manifest.json and changed paths

--- src/pages/index.js
+++ src/pages/index.js
@@ -173,7 +173,7 @@ const Wallet = () => {
 
       <div className="cashu-operations-container">
 
-      <h6>bullishNuts <small>v0.0.18</small></h6>
+      <h6>bullishNuts <small>v0.0.19</small></h6>
       <br></br>
         <div className="section">
           <h2>Balance</h2>
