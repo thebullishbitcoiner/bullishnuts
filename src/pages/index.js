db14b143cd83db035bb6376c1664b505834d21f4
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2024-07-17 19:31:56 -0500

    v0.0.48
    (1) Modified manifest (2) Added nostr.json

--- src/pages/index.js
+++ src/pages/index.js
@@ -555,7 +555,7 @@ const Wallet = () => {
           </div>
         </div>
 
-        <h6>bullishNuts <small>v0.0.47</small></h6>
+        <h6>bullishNuts <small>v0.0.48</small></h6>
         <br></br>
 
         <div className="section">
