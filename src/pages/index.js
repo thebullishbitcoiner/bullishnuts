df1de2dde2640c4499f46c4d897717c72fda1435
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2024-09-14 01:50:23 -0500

    Incremented build number

--- src/pages/index.js
+++ src/pages/index.js
@@ -1003,7 +1003,7 @@ const Wallet = () => {
           </div>
         </div>
 
-        <h6>bullishNuts <small>v0.0.68</small></h6>
+        <h6>bullishNuts <small>v0.0.69</small></h6>
         <br></br>
 
         <div className="section">
