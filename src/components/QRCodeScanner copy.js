15b6f5e3e64c24856447fe602980b034097c74a4
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2024-11-02 17:02:21 -0500

    v0.2.22
    Trying qr-scanner

--- src/components/QRCodeScanner copy.js
+++ src/components/QRCodeScanner copy.js
@@ -1,29 +1,34 @@
-import React, { useState, useEffect, useRef } from 'react';
-import QrScanner from 'qr-scanner';
+import React, { useState, useEffect } from 'react';
+import QrScanner from 'react-qr-scanner';
 import { CrossIcon } from "@bitcoin-design/bitcoin-icons-react/filled";
 
 const QRCodeScanner = ({ onClose = () => { }, isScanQRModalOpen }) => {
     const [data, setData] = useState('No data scanned yet');
     const [isBackCamera, setIsBackCamera] = useState(true); // State to track camera
-    const videoRef = useRef(null); // Reference for the video element
+    const [videoConstraints, setVideoConstraints] = useState({
+        facingMode: { exact: "environment" } // Default to back camera
+    });
 
-    useEffect(() => {
-        // Initialize the QR scanner
-        const qrScanner = new QrScanner(videoRef.current, result => setData(result), {
-            facingMode: isBackCamera ? 'environment' : 'user',
-        });
+    const handleScan = (data) => {
+        if (data) {
+            setData(data);
+        }
+    };
 
-        // Start scanning
-        qrScanner.start();
+    const handleError = (err) => {
+        console.error(err);
+    };
 
-        // Cleanup function to stop the scanner
-        return () => {
-            qrScanner.stop();
-        };
-    }, [isBackCamera]); // Re-run effect when camera mode changes
+    // Custom canvas configuration
+    const canvasprops = {
+        willReadFrequently: true, // Set this attribute to true for performance
+    };
 
     const toggleCamera = () => {
         setIsBackCamera(prev => !prev);
+        setVideoConstraints(prev => ({
+            facingMode: prev.facingMode && prev.facingMode.exact === 'environment' ? { exact: 'user' } : { exact: 'environment' }
+        }));
     };
 
     return (
@@ -32,10 +37,17 @@ const QRCodeScanner = ({ onClose = () => { }, isScanQRModalOpen }) => {
         }}>
             <div className="modal-content">
                 <h2>QR Code Scanner</h2>
-                <button onClick={onClose}>
+                <button onClick={onClose} >
                     <CrossIcon style={{ height: '21px', width: '21px', position: 'absolute', top: '15px', right: '15px' }} />
                 </button>
-                <video ref={videoRef} style={{ width: '100%' }} />
+                <QrScanner
+                    delay={300}
+                    onError={handleError}
+                    onScan={handleScan}
+                    style={{ width: '100%' }}
+                    canvasProps={canvasprops}
+                    videoConstraints={videoConstraints} 
+                />
                 <textarea
                     value={data} // Use value prop instead of children
                     readOnly // Make it read-only if you don't want to allow editing
