6968df51997c5f9158c42dd534b1c2e0f8cb2dff
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2024-11-05 10:55:16 -0600

    v0.2.37
    Moved globals.css to the top

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
 import "@/styles/globals.css";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1353,7 +1353,7 @@ const Wallet = () => {
       <div className="cashu-operations-container">
 
         <div className="app_header">
-          <h2><b><button onClick={() => showConfetti()}>bullishNuts</button></b><small style={{ marginLeft: '3px', marginTop: '1px' }}>v0.2.36</small></h2>
+          <h2><b><button onClick={() => showConfetti()}>bullishNuts</button></b><small style={{ marginLeft: '3px', marginTop: '1px' }}>v0.2.37</small></h2>
           <div id="refresh-icon" onClick={refreshPage}><RefreshIcon style={{ height: '21px', width: '21px' }} /></div>
         </div>
 
