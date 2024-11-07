f58c7a5d9f2bdb2e5fe1ded7747af03e38c1b77f
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2024-11-07 09:17:52 -0600

    v0.2.44
    (1) Tweaked contact icons (2) Added SimpleX link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1330,7 +1330,7 @@ const Wallet = () => {
       <div className="cashu-operations-container">
 
         <div className="app_header">
-          <h2><b><button onClick={() => showConfetti()}>bullishNuts</button></b><small style={{ marginLeft: '3px', marginTop: '1px' }}>v0.2.43</small></h2>
+          <h2><b><button onClick={() => showConfetti()}>bullishNuts</button></b><small style={{ marginLeft: '3px', marginTop: '1px' }}>v0.2.44</small></h2>
           <div id="refresh-icon" onClick={refreshPage}><RefreshIcon style={{ height: '21px', width: '21px' }} /></div>
         </div>
 
@@ -1522,18 +1522,21 @@ const Wallet = () => {
         <br></br>
 
         <div className="centered-container">
-        <div className="button-container" style={{ marginBottom: '15px' }}>
-            <a href="https://github.com/thebullishbitcoiner/bullishnuts">
-              <img src="/images/github-orange-circle.png" alt="GitHub icon" style={{ height: '25px' }} />
-            </a>
+          <div className="button-container" style={{ marginBottom: '15px' }}>
             <a href="https://nostr.com/npub1cashuq3y9av98ljm2y75z8cek39d8ux6jk3g6vafkl5j0uj4m5ks378fhq">
-              <img src="/images/nostr-orange-circle.png" alt="GitHub icon" style={{ height: '25px' }} />
+              <img src="/images/nostr-orange.png" alt="Nostr icon" style={{ height: '33px' }} />
+            </a>
+            <a href="https://simplex.chat/contact#/?v=2-7&smp=smp%3A%2F%2F1OwYGt-yqOfe2IyVHhxz3ohqo3aCCMjtB-8wn4X_aoY%3D%40smp11.simplex.im%2FOdneEQIS5ONmyX_gTFJmAZWTdBi6ueMD%23%2F%3Fv%3D1-3%26dh%3DMCowBQYDK2VuAyEARR3S-y3Gga4bRRi23CIrZYhtg8G21jbKgl861DN9NW0%253D%26srv%3D6ioorbm6i3yxmuoezrhjk6f6qgkc4syabh7m3so74xunb5nzr4pwgfqd.onion">
+              <img src="/images/simplex-svgrepo-com.png" alt="SimpleX icon" style={{ height: '33px' }} />
+            </a>
+            <a href="https://github.com/thebullishbitcoiner/bullishnuts">
+              <img src="/images/github-svgrepo-com.png" alt="GitHub icon" style={{ height: '33px' }} />
             </a>
           </div>
           <p style={{ fontSize: '14px' }}>
             Made with 🐂 by <a href="https://primal.net/thebullishbitcoiner">thebullishbitcoiner</a>
           </p>
-        
+
         </div>
 
       </div>
